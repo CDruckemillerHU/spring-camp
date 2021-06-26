ace21d5bbcb15496d5294b94fbfa395ae3ad692f
Caleb Druckemiller <cdruckemiller@harrisburgu.edu>
2021-06-25 23:12:38 -0400

    Restructure student API layer

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -19,16 +11,6 @@ public class DemoApplication {
 	}
 
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Caleb",
-						"cdruckemiller@harrisburgu.edu",
-						LocalDate.of(1995, Month.NOVEMBER, 11),
-						25)
-		);
-	}
+
 
 }
